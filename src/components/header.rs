3c1f37e52e2b8b0f004971b71230ec7ff7d5de76
itsara <khajer@gmail.com>
2023-12-03 17:10:58 +0700

    change code from enter name

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,78 +1,40 @@
 use gloo::console::log;
 use gloo::utils::document;
+// use std::ops::Deref;
 use wasm_bindgen::JsCast;
 use web_sys::{Element, HtmlInputElement};
 use yew::prelude::*;
 
-#[derive(Properties, PartialEq)]
-pub struct Prop {
-    pub name: String,
-    pub name_change: Callback<User>,
-}
-
-#[derive(Default, Clone)]
-pub struct User {
-    pub name: String,
-}
-
-#[function_component(TextInput)]
-pub fn input_name(prop: &Prop) -> Html {
-    let onchange = Callback::from(|event: Event| {
-        log!("text change");
-        let target = event.target().unwrap();
-        let input = target.unchecked_into::<HtmlInputElement>();
-        log!(input.value());
-    });
-
-    let _namechange_fn = prop.name_change.clone();
-
+#[function_component]
+pub fn Header() -> Html {
+    let username_state = use_state(|| String::from(""));
     let onclick = {
+        let username_state = username_state.clone();
         move |_| {
-            log!("onclick ->> ");
+            log!("click enter");
             let div: Element = document().get_element_by_id("inputname").unwrap();
             let value_input = div.dyn_into::<HtmlInputElement>().unwrap().value();
-            log!(&value_input);
 
-            if &value_input != "" {
-                let user = User {
-                    name: "xxx".to_string(),
-                };
-                _namechange_fn.emit(user);
-                log!("name set up ");
-            } else {
-                log!("name not setup");
-            }
+            let username_state = username_state.clone();
+
+            username_state.set(value_input);
         }
     };
 
     html! {
         <>
-        {"name: "}<input id="inputname" name = {prop.name.clone()} type="text" onchange={onchange} />
-        <button {onclick}>{"enter"}</button>
-        </>
-    }
-}
-
-#[function_component]
-pub fn Header() -> Html {
-    let mut name = String::new();
-    let name_change = Callback::from(|user: User| {
-        log!(">>>> {}", user.name);
-        // name = "test"
-        log!("root control");
-    });
-    html! {
         <div id="header">
-        if name == ""{
+        if username_state.to_string().eq(""){
             <div class="box-name" id="u-name">
-                <TextInput name={name} {name_change}/>
-
+                <input type="text" id="inputname" />
+                <button {onclick}>{"enter"}</button>
             </div>
         }
         else{
-            <div>{name}</div>
+            <div>{username_state.to_string()}</div>
         }
 
         </div>
+        </>
     }
 }
